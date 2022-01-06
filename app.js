bc5ba0027170d2fafc85aa628ed2ed35e544571a
Dan Vo <Dan@DVos-MacBook-Pro.local>
2022-01-05 19:19:11 -0800

    jQuery Movie App

---
+++ app.js
@@ -0,0 +1,39 @@
+
+// when user submits the form, it creates a new table row and resets the form
+
+$("#submit").click(function(e){
+    e.preventDefault();
+
+    let movie = $("#movie-input").val();
+    let rating = $("#ratings-input").val();
+
+    let movieEntry = {movie, rating};
+    const appendEntry = createMovieList(movieEntry);
+
+    $('#table-body').append(appendEntry);
+    $('form').trigger("reset");
+
+    $('.removeBtn').click(function(){
+    $(this).parents('tr').remove();
+})
+})
+
+// function creates the table row with the inputted data
+createMovieList = (entry) => {
+    return `
+        <tr>
+            <td>${entry.movie}</td>
+            <td>${entry.rating}</td>
+            <td>
+                <button class="removeBtn"> X </button>
+            </td>
+        </tr>
+    `;
+}
+
+// when the remove button is clicked on a movie
+$('.removeBtn').click(function(){
+    $(this).parents('tr').remove();
+    // selects the parent of the removeButton which is tr.
+})
+
